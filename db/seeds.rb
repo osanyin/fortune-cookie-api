951265b8829952ebdca95b6f444e468be8852169
Rafael Guimarães <rafaelguimaraesweb@gmail.com>
2015-08-20 20:42:24 -0300

    Inserindo alguns frases para povoar o banco de dados

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Frases retiradas do link: http://www.blogdocasamento.com.br/70-frases-para-seus-biscoitos-da-sorte/
+
+phrases = [
+  { :body => 'A vida trará coisas boas se tiveres paciência.' },
+  { :body => 'Demonstre amor e alegria em todas as oportunidades e verás que a paz nasce dentro de você.' },
+  { :body => 'Não compense na ira o que lhe falta na razão.' },
+  { :body => 'Defeitos e virtudes são apenas dois lados da mesma moeda.' }, 
+  { :body => 'Não há que ser forte. Há que ser flexível.' }, 
+  { :body => 'A maior de todas as torres começa no solo.' }, 
+  { :body => 'Há três coisas que jamais voltam; a flecha lançada, a palavra dita e a oportunidade perdida.' }, 
+]
+
+Phrase.create(phrases)
